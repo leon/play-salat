d0a59a6049455dd527ea0ceef5bc03f1e3c782ba
Leon <leon@radley.se>
2012-04-10 15:06:23 +0200

    OSS Deploy

--- project/Build.scala
+++ project/Build.scala
@@ -12,6 +12,38 @@ object ProjectBuild extends Build {
     resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/",
     resolvers += "Typesafe Snapshots" at "http://repo.typesafe.com/typesafe/snapshots/",
     libraryDependencies += "play" %% "play" % "[2.0,)",
-    libraryDependencies += "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT"
+    libraryDependencies += "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT",
+
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    pomExtra := (
+      <url>http://jsuereth.com/scala-arm</url>
+      <licenses>
+        <license>
+          <name>Apache 2.0</name>
+          <url>http://www.opensource.org/licenses/Apache-2.0</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:leon/play-mongodb.git</url>
+        <connection>scm:git:git@github.com:leon/play-mongodb.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>leon</id>
+          <name>Leon Radley</name>
+          <url>http://leon.radley.se</url>
+        </developer>
+      </developers>
+    ),
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    }
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,8 @@
-resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+resolvers ++= Seq(
+  "sbt-idea-repo" at "http://mpeltonen.github.com/maven/",
+  Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
+)
 
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.0.0")
+
+addSbtPlugin("com.jsuereth" % "xsbt-gpg-plugin" % "0.6")
